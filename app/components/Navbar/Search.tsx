514772e7b89856a5b765a34d8b1b7d8bba7bd7d8
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-04-04 04:01:36 +0600

    navbar 50% complete

---
+++ app/components/Navbar/Search.tsx
@@ -0,0 +1,82 @@
+'use client';
+import React from 'react';
+import { BiSearch } from 'react-icons/bi'
+
+const Search = () => {
+    return (
+        <div
+            className="
+        border-[1px]
+        w-full
+        md:w-auto
+        py-2
+        shadow-sm
+        hover:shadow-md
+        rounded-full
+        cursor-pointer
+        transition
+        "
+        >
+            <div
+                className="
+            flex
+            flex-row
+            justify-between
+            items-center
+            "
+            >
+                <div
+                    className="
+            text-sm
+            font-semibold
+            px-6
+            "
+                >
+                    Anywhere
+                </div>
+                <div
+                    className="
+                hidden
+                sm:block
+                text-sm
+                font-semibold
+                px-6
+                border-x-[1px]
+                flex-1
+                text-center
+                "
+                >
+                    Any Week
+                </div>
+                <div
+                    className="
+                text-sm
+                pl-6
+                pr-2
+                text-gray-600
+                flex
+                items-center
+                gap-3
+                "
+                >
+                    <div className='hidden sm:block'>Add Guests</div>
+                    <div
+                        className="
+                    p-2
+                    bg-rose-500
+                    text-white
+                    rounded-full
+                    "
+                    >
+                        <BiSearch size={18}></BiSearch>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Search;
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl text-indigo-600  font-bold">Hello Next.js by kamrul islam</h1>
+      <h1 className="text-xl text-indigo-600  font-bold">Hello Next.js by kamrul islam</h1>
     </div>
   )
 }
